8d4e62cfc2beff44ca694f5958448bfd905ff82a
Remezov Dmitriy <darknessdizi@gmail.com>
2023-03-11 02:33:56 +0700

    Добавлены права админов

--- api_with_restrictions/advertisements/permissions.py
+++ api_with_restrictions/advertisements/permissions.py
@@ -5,4 +5,7 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == 'GET':
             return True
-        return request.user == obj.creator
+        elif bool(request.user and request.user.is_staff): # проверка является ли пользователь админом
+            return True
+        else:
+            return request.user == obj.creator # проверка является ли пользователь собственником
